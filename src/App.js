0668ec9e11ba5a709ac37a92c963880e972b3f36
marismendi <arismendimauricio@gmail.com>
2020-03-01 22:50:52 -0300

    saving work

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
+function App() {
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
+  );
+}
+
+export default App;
